8f9cf685be870d508c16aa96471f17a98ba9c39d
SurbhiSB <surbhirbhise@gmail.com>
2025-06-26 13:49:19 +0530

    create sign in api route

--- client/src/pages/SignUp.jsx
+++ client/src/pages/SignUp.jsx
@@ -51,15 +51,8 @@ setLoading(false);
     setLoading(false);
     setError(error.message);
   }
-
- 
-  
 };
-
-
-
-
-  return (
+return (
     <div className='p-3 max-w-lg mx-auto'>
       <h1 className='text-3xl text-center font-semibold my-7'>SignUp</h1>
       <form onSubmit={handleSubmit} className='flex flex-col gap-4'>
